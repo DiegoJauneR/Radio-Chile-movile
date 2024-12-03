75bd412f517a64726292f698005c84d40347c2f9
Diego fernando Jaune <diegofernando.jaune@alumnos.ulagos.cl>
2024-12-02 21:52:37 -0300

    Creacion de Repositorio Radio Mobile con Taiwing

---
+++ Radio-Mobile-Chile/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./constants/**/*.{js,jsx,ts,tsx}",
+    "./scripts/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
