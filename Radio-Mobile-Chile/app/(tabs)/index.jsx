4ae62acf63ee36ce9ad52e160fad3e665286429f
David Osorio <dvilla20152025@gmail.com>
2024-12-07 02:32:22 -0300

    Update: Aplication style whith tailwind

--- Radio-Mobile-Chile/app/(tabs)/index.jsx
+++ Radio-Mobile-Chile/app/(tabs)/index.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Button, View, TextInput, TouchableOpacity, Text } from "react-native";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import {
+  Button,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  ImageBackground,
+  Image,
+  SafeAreaView,
+} from "react-native";
 import Radio from "../../components/Radio";
 
 export default function HomeScreen() {
   const [data, setData] = useState([]);
+  const [styleBuscar, setStyleBuscar] = useState(
+    "w-[242] max-h-[300] bg-[#000000b8] text-white border-hidden absolute overflow-hidden top-[58] z-20 border rounded-3xl border-green-600 p-2"
+  );
   const [iterador, setIterador] = useState(2);
   const soundRef = useRef(null); // Para manejar el sonido desde HomeScreen
   const [radiosFiltradas, setRadiosFiltradas] = useState([]);
@@ -58,36 +71,85 @@ export default function HomeScreen() {
     const iteradorBuscado = data.findIndex(iterador2);
     if (iteradorBuscado === -1) return;
     setIterador(iteradorBuscado);
+    setStyleBuscar(
+      "w-[242] max-h-[300] bg-[#000000b8] text-white border-hidden absolute overflow-hidden top-[58] z-20 border rounded-3xl border-green-600 p-2"
+    );
+    setBusqueda();
   };
 
   const rellenarBusqueda = (nombreRadio) => {
-    setBusqueda(nombreRadio);
     setRadiosFiltradas([]);
+    setBusqueda(nombreRadio);
+    setStyleBuscar("hidden");
   };
 
   return (
-    <View>
-      <TextInput
-        onChangeText={filtrarBusqueda}
-        value={busqueda}
-        placeholder="Buscar Radio"
+    <ImageBackground
+      source={{
+        uri: "https://radios-chile.netlify.app/assets/fondo-BqauMuYs.webp",
+      }}
+      resizeMode="cover"
+      className="flex-1 brightness-60"
+    >
+      <Image
+        source={{
+          uri: "https://res.cloudinary.com/doq82xcpd/image/upload/v1728602094/titulo_ekqc6e.webp",
+        }}
+        className="absolute top-[58px] ml-10 mt-9"
+        style={{ width: 300, height: 65 }}
       />
 
-      {busqueda && (
-        <View>
-          {radiosFiltradas.map((radio, index) => (
-            <View key={index}>
-              <TouchableOpacity onPress={() => rellenarBusqueda(radio.name)}>
-                <Text>{radio.name}</Text>
+      <View className="flex justify-center items-center h-screen">
+        <View className="flex-row justify-center w-2/4 mb-5 ">
+          <TextInput
+            onChangeText={filtrarBusqueda}
+            value={busqueda}
+            placeholder="Buscar Radio"
+            placeholderTextColor="white"
+            className="border text-white  border-gray-300 rounded-2xl p-2 w-3/4 mr-1"
+          />
+
+          {busqueda && (
+            <View className={styleBuscar}>
+              {radiosFiltradas.map((radio, index) => (
+                <View key={index} className="mb-2">
+                  <TouchableOpacity
+                    onPress={() => rellenarBusqueda(radio.name)}
+                  >
+                    <Text className="text-white text-center mb-3 ">
+                      {radio.name}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </View>
+          )}
+          <TouchableOpacity onPress={buscarRadio}>
+            <Text className="border border-green-600  text-green-600 px-6 py-2 rounded-3xl">
+              {"Buscar"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View className="w-64  content-center bg-[#090909d6] p-10 border rounded-3xl">
+          <Radio data={data} iterador={iterador} soundRef={soundRef} />
+
+          <View className="absolute left-[45] bottom-11">
+            <View className="flex flex-row mt-4 space-x-4 justify-around gap-9 ">
+              <TouchableOpacity onPress={anteriorRadio}>
+                <Text className=" text-white px-4 py-2 rounded">
+                  <FontAwesome6 name="arrow-left" size={14} color="white" />
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity></TouchableOpacity>
+              <TouchableOpacity onPress={siguienteRadio}>
+                <Text className=" text-white px-4 py-2 rounded">
+                  <FontAwesome6 name="arrow-right" size={14} color="white" />
+                </Text>
               </TouchableOpacity>
             </View>
-          ))}
+          </View>
         </View>
-      )}
-      <Button title="Buscar Radio" onPress={buscarRadio}></Button>
-      <Radio data={data} iterador={iterador} soundRef={soundRef} />
-      <Button title="->" onPress={siguienteRadio}></Button>
-      <Button title="<-" onPress={anteriorRadio}></Button>
-    </View>
+      </View>
+    </ImageBackground>
   );
 }
--- Radio-Mobile-Chile/components/Radio.jsx
+++ Radio-Mobile-Chile/components/Radio.jsx
@@ -1,60 +1,94 @@
-import React, { useEffect } from "react"
-import { Text, View, Image, Button } from "react-native"
+import React, { useEffect, useState } from "react";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import {
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
 import { Audio } from "expo-av";
 
 const Radio = ({ data, iterador, soundRef }) => {
-    if (!data || data.length === 0) {
-        return <Text>Cargando...</Text>
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const elemento = data[iterador];
+
+  const handlePlay = async () => {
+    if (soundRef.current) {
+      return; // Ya se está reproduciendo un sonido
     }
+    try {
+      const { sound } = await Audio.Sound.createAsync(
+        { uri: elemento.stream },
+        { shouldPlay: true }
+      );
+      soundRef.current = sound;
+      setIsPlaying(true);
+    } catch (error) {
+      console.error("Error al reproducir el audio:", error);
+    }
+  };
 
-    const elemento = data[iterador]
+  const handlePause = async () => {
+    if (soundRef.current) {
+      await soundRef.current.pauseAsync();
+      soundRef.current = null;
+      setIsPlaying(false);
+    }
+  };
 
-    const handlePlay = async () => {
-        if (soundRef.current) {
-            return // Ya se está reproduciendo un sonido
-        }
-        try {
-            const { sound } = await Audio.Sound.createAsync(
-                { uri: elemento.stream },
-                { shouldPlay: true }
-            );
-            soundRef.current = sound // Guarda el sonido en soundRef
-        } catch (error) {
-            console.error("Error al reproducir el audio:", error)
-        }
+  useEffect(() => {
+    handlePlay();
+    return () => {
+      if (soundRef.current) {
+        soundRef.current.unloadAsync();
+        soundRef.current = null;
+        setIsPlaying(false);
+      }
     };
+  }, [iterador]);
 
-    const handlePause = async () => {
-        if (soundRef.current) {
-            await soundRef.current.pauseAsync()
-            soundRef.current = null
-        }
-    };
+  if (!data || data.length === 0 || !elemento) {
+    return <Text>Cargando...</Text>;
+  }
 
-    useEffect(() => {
-        handlePlay()
-        return () => {
-            if (soundRef.current) {
-                soundRef.current.unloadAsync()
-                soundRef.current = null
-            }
-        }
-    }, [iterador])
+  return (
+    <View
+      key={elemento.name}
+      className=" flex justify-center content-center items-center gap-y-3"
+    >
+      <View>
+        <Image
+          source={{ uri: elemento.image[200] }}
+          className="w-32 h-32 rounded-full"
+          resizeMode="cover"
+        />
+      </View>
+      <Text className="text-white mb-4 text-xl font-bold ">
+        {elemento.name}
+      </Text>
+      <Text
+        onPress={() => Linking.openURL(elemento.url)}
+        className="text-green-600 text-mb-4"
+      >
+        {elemento.name}
+      </Text>
+      <TouchableOpacity
+        onPress={isPlaying ? handlePause : handlePlay}
+        className="bg-white py-2 px-4 rounded-full border w-[40] h-[40] justify-center "
+      >
+        <Text className="text-white font-bold text-center ">
+          {isPlaying ? (
+            <FontAwesome6 name="pause" size={10} color="black" />
+          ) : (
+            <FontAwesome6 name="play" size={10} color="black" />
+          )}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-    return (
-        <View key={elemento.name}>
-            <View>
-                <Image
-                    source={{ uri: elemento.image[200] }}
-                    className="w-32 h-32 rounded-full"
-                    resizeMode="cover"
-                />
-            </View>
-            <Text>{elemento.name}</Text>
-            <Button title="Play" onPress={handlePlay} />
-            <Button title="Pause" onPress={handlePause} />
-        </View>
-    )
-}
-
-export default Radio
+export default Radio;
